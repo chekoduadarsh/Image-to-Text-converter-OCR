f74d1dd21c1fb27ce13da8e7b209aa98bc81fcb2
ADARSH C <chekodu.adarsh@gmail.com>
2018-03-06 22:23:11 +0530

    Add files via upload

---
+++ TesseractOCR.py
@@ -0,0 +1,42 @@
+import cv2
+import numpy as np
+import pytesseract
+from PIL import Image
+
+# Path of working folder on Disk
+src_path = "E:/Lab/Python/Project/OCR/"
+
+def get_string(img_path):
+    # Read image with opencv
+    img = cv2.imread(img_path)
+
+    # Convert to gray
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Apply dilation and erosion to remove some noise
+    kernel = np.ones((1, 1), np.uint8)
+    img = cv2.dilate(img, kernel, iterations=1)
+    img = cv2.erode(img, kernel, iterations=1)
+
+    # Write image after removed noise
+    cv2.imwrite(src_path + "removed_noise.png", img)
+
+    #  Apply threshold to get image with only black and white
+    #img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
+
+    # Write the image after apply opencv to do some ...
+    cv2.imwrite(src_path + "thres.png", img)
+
+    # Recognize text with tesseract for python
+    result = pytesseract.image_to_string(Image.open(src_path + "thres.png"))
+
+    # Remove template file
+    #os.remove(temp)
+
+    return result
+
+
+print ('--- Start recognize text from image ---')
+print (get_string(src_path + "2.png"))
+
+print ("------ Done -------")
---
+++ visionapiocr.py
@@ -0,0 +1,46 @@
+import io
+import os
+import os
+from base64 import b64encode
+from os import makedirs
+from os.path import join, basename
+from sys import argv
+import json
+import requests
+from google.cloud.vision import types
+from google.cloud.bigquery.client import Client
+import io
+import os
+
+# Imports the Google Cloud client library
+from google.cloud import vision
+from google.cloud.vision import types
+
+
+
+def detect_text(path):
+    """Detects text in the file."""
+    client = vision.ImageAnnotatorClient()
+
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = types.Image(content=content)
+
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    #print(texts)
+    print('Texts:')
+
+    for text in texts:
+        a=("{}".format(text.description))
+        print(a)
+        #vertices = (['({},{})'.format(vertex.x, vertex.y)
+        #            for vertex in text.bounding_poly.vertices])
+
+        #print('bounds: {}'.format(','.join(vertices)))'''
+    print((a))
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '' #give path to your Service account keys .json file
+bq_client = Client()
+path=''    					  #give path to your image
+detect_text(path)
